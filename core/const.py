d20551f2e36014d087caef01bc6c4279973e6a1e
Edward Tapiero <edwardtapiero28@gmail.com>
2022-03-30 21:41:52 -0500

    initial commit

---
+++ core/const.py
@@ -0,0 +1,9 @@
+URL_MELI = 'https://listado.mercadolibre.com.ar/celular-smarphones'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36'
+
+RESET_SEARCH = False
+PAGINATION = 5
+BRAND = 'Samsung'
+
+HOST = "0.0.0.0"
+PORT = 5002
